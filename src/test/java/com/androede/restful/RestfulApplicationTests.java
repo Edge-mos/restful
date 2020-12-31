6ccb60fef69a7175049df305b099fbf77cb45f0b
Edge <Androedge@gmail.com>
2020-12-31 15:20:52 +0300

    Initial commit

---
+++ src/test/java/com/androede/restful/RestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.androede.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
